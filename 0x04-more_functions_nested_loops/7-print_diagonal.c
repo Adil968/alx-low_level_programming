d5232110061541d544546ad39e06be25d9907094
Adil968 <elazzouzyadil6@gmail.com>
2023-06-23 17:59:45 -0700

    ccc

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,18 +7,18 @@
  */
 void print_diagonal(int n)
 {
-    int postn, space;
+	int postn, space;
 
-    if (n <= 0)
-        _putchar('\n');
-    else
-    {
-        for (postn = 1; postn <= n; ++postn)
-        {
-            for (space = 1; space <= postn; ++space)
-                _putchar(' ');
-            _putchar('\\');
-            _putchar('\n');
-        }
-    }
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (postn = 1; postn <= n; ++postn)
+		{
+			for (space = 1; space <= postn; ++space)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
 }
